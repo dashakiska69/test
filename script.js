aa2ecda4cc668cd48896569a5e11437317c27f0b
dashakiska69 <dashakiska69@gmail.com>
2020-02-03 02:06:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+a = 5;
+console.log(a);
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let incr = 10,
+decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+isClose = false;
+
+console.log(!isChecked || isClose);
+
