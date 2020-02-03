b3c868ef04d78a3c7551bb04684eaca30703b216
dashakiska69 <dashakiska69@gmail.com>
2020-02-03 12:45:22 +0300

    2 commit

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+'use strict';
 
 
+'use strict';
 
-a = 5;
-console.log(a);
+let money = prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let answer1 = prompt('Введите обязательную статью расходов в этом месяцев');
+let answer2 = prompt('Во сколько обойдется?');
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {answer1: answer1,
+                answer2: answer2},
+    optionalExpenses: null,
+    income: null,
+    savings: false
+};
+
+console.log((appData.budget - appData.expenses.answer1 - appData.expenses.answer2)/30);
+/*
 var number = 5;
 var string = "Hello";
 var sym = Symbol();
@@ -26,3 +43,39 @@ isClose = false;
 
 console.log(!isChecked || isClose);
 
+if (2*3 == 8) {
+    console.log("Верно");
+} else {
+    console.log('ne verno');
+}
+
+let num = 50;
+
+if (num < 49) {
+    console.log('ne verno');
+} else if (num > 100) {
+    console.log('mnogo');
+} else {
+    console.log('eto tak');
+};
+
+(num == 50) ? console.log('verno') : console.log('neverno');
+
+switch (num) {
+    case num < 49:
+        console.log('ne verno');
+        break;
+    case num >100:
+        console.log('mnogo');
+        break;
+    case num > 80:
+        console.log('mnogo');
+        break;
+    case num == 50:
+        console.log('verno');
+        break;
+    default:
+        console.log('chto to poshlo ne tak');
+        break;
+}
+*/
